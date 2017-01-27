5c8bfffa1a242472046b12ad8ec1e8133dd6d91f
zeke farioli <efarioli@gmail.com>
2017-01-27 10:15:04 +0000

    adding Select Controls with Ng-Options and grouping

--- controller.js
+++ controller.js
@@ -3,6 +3,9 @@ angular.module('eventApp')
 
 	this.event = eventFactory.getAllEvents();
 
+	this.categories = [{id:1, name:'Music', group:'Main'},{id:2, name:'Cinema', group:'Main'},
+	{id:3, name:'Games', group:'Second'},{id:4, name:'Race', group:'Second'}];
+
 	this.submitForm = function(form){
 
 		eventFactory.createEvent(angular.copy(form), this.event);
