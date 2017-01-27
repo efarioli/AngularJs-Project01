9313afffd424959081183d47562f9a14148520d5
zeke farioli <efarioli@gmail.com>
2017-01-27 16:18:29 +0000

    radio buttons and checkboxes

--- controller.js
+++ controller.js
@@ -1,12 +1,54 @@
 angular.module('eventApp')
 .controller('formCtrl', ['eventFactory',  function (eventFactory) {
 
+	var self = this;
+
 	this.event = eventFactory.getAllEvents();
 
 	this.categories = [{id:1, name:'Music'},{id:2, name:'Cinema'},
 	{id:3, name:'Games'},{id:4, name:'Race'}];
 
 	this.selectedOption = {id:1, name:'Music'};
+
+	this.specialEvent = true;
+
+	this.specialValue ={id:1, value:'somethingSpecial'};
+
+	//Initialize Special types checkboxes
+
+	this.specialType = [{name: 'Age Restricted', checked: false},{name: 'Luxury', checked: false}];
+
+	this.selectAllTypes = function (){
+
+		if (self.bothSelected){
+			self.bothSelected = true;
+		} else {
+			self.bothSelected = false;
+		}
+
+		angular.forEach(this.specialType, function(item){
+
+			item.checked = self.bothSelected;
+		})
+
+		}
+
+
+
+
+
+
+	/*	var big;
+if (x > 10) {
+    big = true;
+}
+else {
+    big = false;
+}
+
+var big = (x > 10) ? true : false;
+
+	};*/
 
 	this.submitForm = function(form){
 
